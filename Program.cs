e2352843bd04dd8718868dd86dd0eafcd2bb3fbd
asanyx <nickusx@gmail.com>
2020-10-23 13:09:54 +0200

    pierwsza wersja

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] magazyn = new string[5, 2];
+
+            Console.WriteLine("Wprowadź dane: ");
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    magazyn[i, j] = Console.ReadLine();
+
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("{0,6}", magazyn[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
